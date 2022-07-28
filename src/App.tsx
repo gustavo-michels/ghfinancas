d4ef178c3e8c58b0c13f7ede7e0bbad4e8fd9c68
Gustavo H. Michels <ghmichels@gmail.com>
2022-07-28 17:22:29 -0300

    feat: add transactions table and new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import { Dashboard } from './components/dashboard'
 import { Header } from './components/Header'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      ></NewTransactionModal>
     </>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content, LogoContainer } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
@@ -9,7 +13,9 @@ export function Header() {
           <img src={logoImg} alt="gh finanças" />
           <h1>gh finanças</h1>
         </LogoContainer>
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   )
